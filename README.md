62c5218d65e105accc8e4efba7e327bc9cfefeef
Astrid Caballero-Pardo <astridcaballero@gmail.com>
2026-07-06 15:09:05 +0100

    Get familiar with Boto Add README

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ![img.png](img.png)
 ## Module 14 - Automation with Python
 
-### Chapter 2 - Install Boto3 and connect to AWS
+### Chapter 3 - Getting familiar with Boto
 
-Notes from chapter 2 were taken in notes app in freestyle, and it includes a mix of screenshots of TWN bootcamp as well as
+Notes from chapter 3 were taken in notes app in freestyle, and it includes a mix of screenshots of TWN bootcamp as well as
 screenshots of my own work on the terminal and browser. I used AI and I have added that research to my notes.
 
 Notes have been exported into pdf format to be able to get the screenshots (avoiding retaking screenshots which is time-consuming)
 
 You can find original notes for
-- Chapter 2 [here](Module%2014%20-%20chapter%202_notes.pdf).
+- Chapter 3 [here](Module%2014%20-%20chapter%203_notes.pdf).
--- main.py
+++ main.py
@@ -1,1 +1,39 @@
 import boto3
+
+ec2_client = boto3.client('ec2', region_name='eu-west-3')
+ec2_resource = boto3.resource('ec2', region_name='eu-west-3')
+
+new_vpc = ec2_resource.create_vpc(
+    CidrBlock="10.0.0.0/16"
+)
+
+new_vpc.create_subnet(
+    CidrBlock="10.0.1.0/24"
+)
+
+new_vpc.create_subnet(
+    CidrBlock="10.0.2.0/24"
+)
+
+new_vpc.create_tags(
+    Tags=[
+            {
+                'Key': 'Name',
+                'Value': 'my-vpc'
+            },
+        ]
+)
+
+
+all_available_vpcs = ec2_client.describe_vpcs()
+vpcs = all_available_vpcs["Vpcs"]
+
+# loop the list of Vpcs which is a list of dictionaries
+for vpc in vpcs:
+    # in a dictionary I get a value using its key name
+    print(vpc["VpcId"])
+    cidr_block_assoc_sets = vpc["CidrBlockAssociationSet"]
+    # iterate over the list
+    for assoc_set in cidr_block_assoc_sets:
+        print(assoc_set["CidrBlockState"])
+
