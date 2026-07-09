3cdd650fc693e714e979170ee2aa81ad37197443
Astrid Caballero-Pardo <astridcaballero@gmail.com>
2026-07-09 15:03:50 +0100

    Handle errors and add README

--- volume-backups.py
+++ volume-backups.py
@@ -13,10 +13,13 @@ def create_volume_snapshots():
         ]
     )
     for volume in volumes['Volumes']:
-        new_snapshot = ec2_client.create_snapshot(
-            VolumeId=volume['VolumeId']
-        )
-        print(new_snapshot)
+        try:
+            new_snapshot = ec2_client.create_snapshot(
+                VolumeId=volume['VolumeId']
+            )
+            print(new_snapshot)
+        except:
+            # handle error here
 
 schedule.every(20).seconds.do(create_volume_snapshots)
 
