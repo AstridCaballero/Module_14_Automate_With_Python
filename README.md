852f30b2f508b2c26d015ea24925e9f0b7a0b9e9
Astrid Caballero-Pardo <astridcaballero@gmail.com>
2026-07-09 14:18:53 +0100

    Restore volume from snapshots and add README

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ![img.png](img.png)
 ## Module 14 - Automation with Python
 
-### Chapter 10 - Automate cleanup of old Snapshots
+### Chapter 11 - Automate restoring EC2 Volume from the Backup
 
-Notes from chapter 10 were taken in notes app in freestyle, and it includes a mix of screenshots of TWN bootcamp as well as
+Notes from chapter 11 were taken in notes app in freestyle, and it includes a mix of screenshots of TWN bootcamp as well as
 screenshots of my own work on the terminal and browser. I used AI and I have added that research to my notes.
 
 Notes have been exported into pdf format to be able to get the screenshots (avoiding retaking screenshots which is time-consuming)
 
 You can find original notes for
-- Chapter 10 [here](Module%2014%20-%20chapter%2010_notes.pdf).
+- Chapter 11 [here](Module%2014%20-%20chapter%2011_notes.pdf).
---
+++ restore-volume.py
@@ -0,0 +1,65 @@
+from operator import itemgetter
+
+import boto3
+
+ec2_client = boto3.client('ec2', region_name="eu-west-2")
+ec2_resource = boto3.resource('ec2', region_name="eu-west-2")
+
+instance_id = "i-0368a9527a5666fdf"
+
+volumes = ec2_client.describe_volumes(
+    Filters=[
+        {
+            'Name': 'attachment.instance-id',
+            'Values': [
+                instance_id,
+            ]
+        },
+    ],
+)
+
+instance_volume = volumes['Volumes'][0]
+
+snapshots = ec2_client.describe_snapshots(
+    OwnerIds=['self'],
+    Filters=[
+        {
+            'Name': 'volume-id', # from describe_snapshots()
+            'Values': [
+                instance_volume['VolumeId'], # from describe_volumes()
+            ]
+        },
+    ]
+)
+
+latest_snapshot = sorted(snapshots['Snapshots'], key=itemgetter('StartTime'), reverse=True)[0]
+print(latest_snapshot['StartTime'])
+
+new_volume = ec2_client.create_volume(
+    SnapshotId=latest_snapshot['SnapshotId'],
+    AvailabilityZone="eu-west-2c", # this can be improved by getting the AZ from the instance
+    TagSpecifications = [
+        {
+            'ResourceType': 'volume',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': 'prod'
+                },
+            ]
+        },
+    ],
+)
+
+while True:
+    vol = ec2_resource.Volume(new_volume['VolumeId'])
+    print(vol.state)
+    if vol.state == 'available':
+        ec2_resource.Instance(instance_id).attach_volume(
+            VolumeId=new_volume['VolumeId'],
+            Device='/dev/xvdb'
+        )
+        break
+
+
+
