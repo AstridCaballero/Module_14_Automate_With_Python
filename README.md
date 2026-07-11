54ebf4589c75661a734998e413b725f292da5456
Astrid Caballero-Pardo <astridcaballero@gmail.com>
2026-07-11 14:07:45 +0100

    Monitor server access is succesful and add README

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ![img.png](img.png)
 ## Module 14 - Automation with Python
 
-### Chapter 13 - Website Monitoring 1: Scheduled Task to Monitor Application Health
+### Chapter 14 - Website Monitoring 2: Automated Email Notification
 
-Notes from chapter 13 were taken in notes app in freestyle, and it includes a mix of screenshots of TWN bootcamp as well as
+Notes from chapter 14 were taken in notes app in freestyle, and it includes a mix of screenshots of TWN bootcamp as well as
 screenshots of my own work on the terminal and browser. I used AI and I have added that research to my notes.
 
 Notes have been exported into pdf format to be able to get the screenshots (avoiding retaking screenshots which is time-consuming)
 
 You can find original notes for
-- Chapter 13 [here](Module%2014%20-%20chapter%2013_notes.pdf).
+- Chapter 14 [here](Module%2014%20-%20chapter%2014_notes.pdf).
--- monitor-website.py
+++ monitor-website.py
@@ -1,8 +1,29 @@
 import requests
+import smtplib
+import os
 
-response = requests.get("http://172-236-15-151.ip.linodeusercontent.com:8080/")
+EMAIL_ADDRESS = os.environ.get("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
-if response.status_code == 200:
-    print("Application is running successfully!")
-else:
-    print("Application Down. Fix it!")
+def send_notification(email_msg):
+    with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        message = f"Subject: SITE DOWN\n{email_msg}"
+        smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, message)
+
+try:
+    response = requests.get("http://172-236-15-151.ip.linodeusercontent.com:8080/")
+    if response.status_code == 200:
+        print("Application is running successfully!")
+    else:
+        print("Application Down. Fix it!")
+        # send notification email to support
+        msg = f"Application returned {response.status_code}"
+        send_notification(msg)
+except Exception as ex:
+    print(f"Connection error happended: {ex}")
+    # send notification email to support
+    msg = "Application not accessible at all."
+    send_notification(msg)
