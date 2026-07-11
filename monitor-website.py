3489a9e24cac6951a8287792d527e93687541198
Astrid Caballero-Pardo <astridcaballero@gmail.com>
2026-07-11 16:49:34 +0100

    Restart app container

--- monitor-website.py
+++ monitor-website.py
@@ -1,6 +1,7 @@
 import requests
 import smtplib
 import os
+import paramiko
 
 EMAIL_ADDRESS = os.environ.get("EMAIL_ADDRESS")
 EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
@@ -13,6 +14,7 @@ def send_notification(email_msg):
         message = f"Subject: SITE DOWN\n{email_msg}"
         smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, message)
 
+
 try:
     response = requests.get("http://172-236-15-151.ip.linodeusercontent.com:8080/")
     if response.status_code == 200:
@@ -22,6 +24,16 @@ try:
         # send notification email to support
         msg = f"Application returned {response.status_code}"
         send_notification(msg)
+
+        # restart the application
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect("172.236.15.151", username="root", key_filename="/Users/astrid/.ssh/id_ed25519")
+        stdin, stdout, stderr = ssh.exec_command("docker start 52527a5428e8")
+        print(stdout.readlines())
+        ssh.close()
+        print("Application restarted")
+        
 except Exception as ex:
     print(f"Connection error happended: {ex}")
     # send notification email to support
