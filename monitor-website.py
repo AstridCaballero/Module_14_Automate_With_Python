4f4726ec9ee342f52ad9f6dcc82218e6940f65b0
Astrid Caballero-Pardo <astridcaballero@gmail.com>
2026-07-11 19:07:31 +0100

    Reboot server and add scheduler

--- monitor-website.py
+++ monitor-website.py
@@ -2,11 +2,31 @@ import requests
 import smtplib
 import os
 import paramiko
+import linode_api4
+import time
+import schedule
 
 EMAIL_ADDRESS = os.environ.get("EMAIL_ADDRESS")
 EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+LINODE_TOKEN = os.environ.get("LINODE_TOKEN")
+
+def restart_server_and_container():
+    # restart linode server
+    print("Rebooting the server...")
+    client = linode_api4.LinodeClient(LINODE_TOKEN)
+    nginx_server = client.load(linode_api4.Instance, 100597079)
+    nginx_server.reboot()
+
+    # restart the application
+    while True:
+        nginx_server = client.load(linode_api4.Instance, 100597079)
+        if nginx_server.status == "running":
+            time.sleep(5)
+            restart_container()
+            break
 
 def send_notification(email_msg):
+    print("Sending email...")
     with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
         smtp.starttls()
         smtp.ehlo()
@@ -14,28 +34,37 @@ def send_notification(email_msg):
         message = f"Subject: SITE DOWN\n{email_msg}"
         smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, message)
 
+def restart_container():
+    print("Restarting the application...")
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect("172.236.15.151", username="root", key_filename="/Users/astrid/.ssh/id_ed25519")
+    stdin, stdout, stderr = ssh.exec_command("docker start 52527a5428e8")
+    print(stdout.readlines())
+    ssh.close()
 
-try:
-    response = requests.get("http://172-236-15-151.ip.linodeusercontent.com:8080/")
-    if response.status_code == 200:
-        print("Application is running successfully!")
-    else:
-        print("Application Down. Fix it!")
+def monitor_application():
+    try:
+        response = requests.get("http://172-236-15-151.ip.linodeusercontent.com:8080/")
+        if response.status_code == 200:
+            print("Application is running successfully!")
+        else:
+            print("Application Down. Fix it!")
+            # send notification email to support
+            msg = f"Application returned {response.status_code}"
+            send_notification(msg)
+            restart_container()
+
+    except Exception as ex:
+        print(f"Connection error happended: {ex}")
         # send notification email to support
-        msg = f"Application returned {response.status_code}"
+        msg = "Application not accessible at all."
         send_notification(msg)
+        restart_server_and_container()
 
-        # restart the application
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect("172.236.15.151", username="root", key_filename="/Users/astrid/.ssh/id_ed25519")
-        stdin, stdout, stderr = ssh.exec_command("docker start 52527a5428e8")
-        print(stdout.readlines())
-        ssh.close()
-        print("Application restarted")
-        
-except Exception as ex:
-    print(f"Connection error happended: {ex}")
-    # send notification email to support
-    msg = "Application not accessible at all."
-    send_notification(msg)
+schedule.every(5).minutes.do(monitor_application)
+
+while True:
+    schedule.run_pending()
+
+
